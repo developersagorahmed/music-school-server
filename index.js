edf5c430f6254adfcd3084589763b2929582a9e2
Sagor Ahmed <developersagorahmed@gmail.com>
2023-06-10 04:38:39 +0600

    added delet by id api make

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7is7xhq.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -80,6 +80,14 @@ async function run() {
 				sort: { students: -1 },
 			};
 			const result = await classesCollection.find(query, options).toArray();
+			res.send(result);
+		});
+		// delete items api
+		app.delete("/myselectedclass/:id", async (req, res) => {
+			const id = req.params.id;
+			console.log(id);
+			const query = { _id: new ObjectId(id) };
+			const result = await selectCollection.deleteOne(query);
 			res.send(result);
 		});
 
